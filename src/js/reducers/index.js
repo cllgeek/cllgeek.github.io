9daaf132de20052067140014af5eed9de1d72208
cllgeek <375587295@qq.com>
2016-10-25 14:23:09 +0800

    add immutable

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,4 +1,5 @@
 // import { combineReducers } from 'redux';
+import objectAssign from 'object-assign';
 import { FETCH_ISSUES, RECEIVE_ISSUES } from '../constants/ActionTypes.js';
 import { fetchIssues, receiveIssues } from '../actions/index.js';
 import SImmutable from 'seamless-immutable';
@@ -9,21 +10,40 @@ var defaultIssuesState = SImmutable({
 });
 
 // issues reducer
-function postIssues(state=defaultIssuesState, action) {
+// function postIssues(state=defaultIssuesState, action) {
+//   switch (action.type) {
+//     case FETCH_ISSUES:
+//       // 获取issues
+//       return state.set('isFetching',true);
+
+//     case RECEIVE_ISSUES:
+//       // 接收issues
+//       return state.merge({
+//         isFetching: false,
+//         items: action.posts
+//       });
+
+//     default:
+//       return state;
+//   }
+// }
+function postIssues(defaultIssuesState, action) {
   switch (action.type) {
     case FETCH_ISSUES:
       // 获取issues
-      return state.set('isFetching',true);
+      return objectAssign({}, defaultIssuesState, {
+        isFetching: true
+      });
 
     case RECEIVE_ISSUES:
       // 接收issues
-      return state.merge({
+      return objectAssign({}, defaultIssuesState, {
         isFetching: false,
         items: action.posts
       });
 
     default:
-      return state;
+      return defaultIssuesState;
   }
 }
 
