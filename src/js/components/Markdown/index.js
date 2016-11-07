d9d27fb72fca7d4f0ae7fba86dc3b11be26fb241
cllgeek <375587295@qq.com>
2016-11-07 15:25:44 +0800

    add markdown component

--- src/js/components/Markdown/index.js
+++ src/js/components/Markdown/index.js
@@ -8,7 +8,7 @@ var DisplayContainer = React.createClass({
     },
     getInitialState:function(){
         return{
-            value:'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Herman Fassett](http://freecodecamp.com/hermanfassett)*'
+            value:'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n ---- *[cll`s blog](cllgeek.github.io)*'
         }
     },
     rawMarkup: function(value) {
